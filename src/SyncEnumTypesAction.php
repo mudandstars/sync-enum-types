36d50b41e10f301eec4690f96631eced914e2d55
Paul Sochiera <paul@sochiera.com>
2023-05-01 13:42:54 +0200

    feat: .d.ts files have correct contents

--- src/SyncEnumTypesAction.php
+++ src/SyncEnumTypesAction.php
@@ -49,7 +49,7 @@ class SyncEnumTypesAction
     {
         $typeName = trim($filePath, '.php');
 
-        $destinationPath =config('sync-enum-types.TYPESCRIPT_ENUM_FOLDER_DESTINATION').$typeName.'.d.ts';
+        $destinationPath = config('sync-enum-types.TYPESCRIPT_ENUM_FOLDER_DESTINATION').'/'.$typeName.'.d.ts';
 
         file_put_contents($destinationPath, $this->stubContents($typeName, $values));
     }
@@ -65,6 +65,6 @@ class SyncEnumTypesAction
         $valuesAsStrings = array_map(fn ($value) => "'".$value."'", $values);
         $possibleTypesString = implode(' | ', $valuesAsStrings);
 
-        return $contents.$possibleTypesString.';';
+        return preg_replace('/\r|\n|"""/', '', $contents).$possibleTypesString.';';
     }
 }
--- tests/CreatesTypescriptFilesTest.php
+++ tests/CreatesTypescriptFilesTest.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Artisan;
 
-it('command creates typescript files in config location', function () {
+test('command creates typescript files in config location', function () {
     $FILE_BASECOUNT = 2;
 
     expect(count(scandir(config('sync-enum-types.TYPESCRIPT_ENUM_FOLDER_DESTINATION'))))->toBe($FILE_BASECOUNT);
@@ -10,4 +10,16 @@ it('command creates typescript files in config location', function () {
     Artisan::call('sync-enum-types');
 
     expect(count(scandir(config('sync-enum-types.TYPESCRIPT_ENUM_FOLDER_DESTINATION'))))->toBe(count(scandir(config('sync-enum-types.TYPESCRIPT_ENUM_FOLDER_DESTINATION'))));
+});
+
+test('files have correct contents', function () {
+    Artisan::call('sync-enum-types');
+
+    $typescriptDirPath = config('sync-enum-types.TYPESCRIPT_ENUM_FOLDER_DESTINATION');
+
+    $enum1Content = file_get_contents($typescriptDirPath.'/Enum1.d.ts');
+    expect($enum1Content)->toEqual("export type Enum1 = 'first case' | 'second case';");
+
+    $enum2Content = file_get_contents($typescriptDirPath.'/Enum2.d.ts');
+    expect($enum2Content)->toEqual("export type Enum2 = 'first case second enum' | 'second case second enum';");
 });
