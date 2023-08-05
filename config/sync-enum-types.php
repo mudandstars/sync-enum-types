80433dd72f8309333d029eb8a12e6ec5241bbed8
Paul Sochiera <paul@sochiera.com>
2023-08-05 15:44:22 +0200

    feat: added type coverage test

--- config/sync-enum-types.php
+++ config/sync-enum-types.php
@@ -3,5 +3,6 @@
 return [
     'PHP_ENUM_FOLDER_DESTINATION' => app_path('Enum'),
     'TYPESCRIPT_ENUM_FOLDER_DESTINATION' => app_path('../resources/ts/types/Enum'),
+    'SYNC_CASES' => true,
     'EXCEPTIONS' => [],
 ];
