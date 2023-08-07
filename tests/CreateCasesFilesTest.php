f1dc1ce0ba7919f5da072af0663b05b330397325
Paul Sochiera <paul@sochiera.com>
2023-08-07 10:47:33 +0200

    fix:  parsing bug

--- tests/CreateCasesFilesTest.php
+++ tests/CreateCasesFilesTest.php
@@ -56,3 +56,22 @@ it('properly imports the type', function () {
     $enum2Content = file_get_contents($casesPath.'/Enum2.ts');
     expect($enum2Content)->toContain("import { Enum2 } from '".$importBasePath."/Enum2';\n");
 });
+
+it('files are entirely correct', function () {
+    Config::set('sync-enum-types.SYNC_CASES', true);
+    Config::set('sync-enum-types.TYPESCRIPT_ENUM_FOLDER_DESTINATION', app_path('../resources/ts/types/Enum'));
+    Config::set('sync-enum-types.CASES_FOLDER_DESTINATION', app_path('../resources/ts/EnumCases'));
+
+    Artisan::call('sync-enum-types');
+
+    $typesPath = config('sync-enum-types.TYPESCRIPT_ENUM_FOLDER_DESTINATION');
+    $casesPath = config('sync-enum-types.CASES_FOLDER_DESTINATION');
+
+    $importBasePath = RelativePathAction::execute($casesPath, $typesPath);
+
+    $enum1Content = file_get_contents($casesPath.'/Enum1.ts');
+    expect($enum1Content)->toContain("import { Enum1 } from '".$importBasePath."/Enum1';\nexport const Enum1Cases: Array<Enum1> = [\n\t'first case',\n\t\"second's case\",\n];\n");
+
+    $enum2Content = file_get_contents($casesPath.'/Enum2.ts');
+    expect($enum2Content)->toContain("import { Enum2 } from '".$importBasePath."/Enum2';\nexport const Enum2Cases: Array<Enum2> = [\n\t'first case second enum',\n\t'second case second enum',\n];\n");
+});
